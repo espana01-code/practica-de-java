16f74d061e9dcdfff5d31ed37aaddc844de7aca7
espana01-code <cespna@unipacifico.edu.co>
2025-08-24 15:18:57 -0500

    Le una actualizacion

--- Operadores.java
+++ Operadores.java
@@ -18,6 +18,14 @@ public class Operadores {
         System.out.println("división: " + (a / b));
         System.out.println("módulo: " + (a % b));
 
+        int x = 20;
+        int y = 23;
+        System.out.println("El resultado de la suma es: " + (x + y));
+        System.out.println("El resultado de la resta es: " + (x - y));
+        System.out.println("El resultado de la multiplcacion es: " + (x * y));
+        System.out.println("El resultado de la divicion es: " + (x / y));
+        System.out.println("El resultado de la modulo es: " + (x % y));
+
     }
 
 }
