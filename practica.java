dac534602d15d14d0253b7247dbe7de166a85cad
espana01-code <cespna@unipacifico.edu.co>
2025-08-24 10:12:12 -0500

    le agrege una suma

--- practica.java
+++ practica.java
@@ -1,5 +1,10 @@
 public class practica {
     public static void main(String[] args) {
         System.out.println("Hola Mundo a todos");
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + suma);
+
     }
 }
