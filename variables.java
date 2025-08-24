64f00cd4ceb1b861e413903a62d3af93abf1daca
espana01-code <cespna@unipacifico.edu.co>
2025-08-24 11:16:19 -0500

    mas practicas

--- variables.java
+++ variables.java
@@ -1,5 +1,18 @@
-public class variables {
+public class Variables {
     public static void main(String[] args) {
         System.out.println("Hola Mundo a todos");
+
+        // int → números enteros.
+        // double → decimales.
+        // String → texto.
+        // boolean → verdadero o falso.
+        int edad = 18;
+        String nombre = "carlos";
+        double estatura = 1.68;
+        boolean estudiantes = true;
+        System.out.println("Me llamo " + nombre + " y tengo " + edad + " años");
+        System.err.println("mi estatura es " + estatura + " metros");
+        System.err.println("¿Soy estudiante de la univercidad" + estudiantes);
+
     }
 }
