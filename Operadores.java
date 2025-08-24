caabef3d506dd1e40bd9ca4a40239cbe03350958
espana01-code <cespna@unipacifico.edu.co>
2025-08-24 11:36:49 -0500

    Agregué el archivo Operadores.java

---
+++ Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        // + (suma)
+
+        // - (resta)
+
+        // * (multiplicación)
+
+        // / (división)
+
+        // % (módulo: el residuo de una división)
+        int a = 10;
+        int b = 3;
+        System.out.println("suma: " + (a + b));
+        System.out.println("resta: " + (a - b));
+        System.out.println("multipicacion: " + (a * b));
+        System.out.println("división: " + (a / b));
+        System.out.println("módulo: " + (a % b));
+
+    }
+
+}
